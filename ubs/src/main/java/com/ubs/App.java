e8e78b72f0c8c481ff54a30a67051b1b95927894
Başak Akan <basak.akan1@gmail.com>
2018-05-18 21:30:28 +0300

    Add files via upload

---
+++ ubs/src/main/java/com/ubs/App.java
@@ -0,0 +1,51 @@
+package com.ubs;
+
+import java.sql.*;
+
+import com.mysql.cj.xdevapi.Statement;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class App extends Application  {
+	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root,700,600);
+	//		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+    public static void main( String[] args ) throws SQLException
+    {
+    	launch(args);
+    	
+//    	 try {
+//             Class.forName("com.mysql.jdbc.Driver");
+//             Connection con = DriverManager.getConnection(
+//                     "jdbc:mysql://160.153.128.35:3306/UbsProject", "thehavays", "ubs12345");
+// //here sonoo is database name, root is username and password
+//             Statement stmt = con.createStatement();
+//             ResultSet rs = stmt.executeQuery("select * from Persons");
+//             while (rs.next())
+//                 System.out.println(rs.getString(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4));
+//             con.close();
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+    
+    }
+}
---
+++ ubs/src/main/java/com/ubs/Controllers/LoginController.java
@@ -0,0 +1,5 @@
+package com.ubs.Controllers;
+
+public class LoginController {
+
+}
